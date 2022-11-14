91e672620d934f288b6284222a74f2e7d6d2a7e6
Cristian <cristianlr3009@gmail.com>
2022-11-14 12:10:40 -0600

    Ruta amigable

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Product extends Model
 {
@@ -15,4 +16,12 @@ class Product extends Model
         'description',
         'thumbnail'
     ];
+
+    protected static function booted()
+    {
+        static::creating(function ($product) {
+            $slug = Str::slug($product->name);
+            $product->slug = Product::where('slug', $slug)->exists() ? ($slug . uniqid()) : $slug;
+        });
+    }
 }
